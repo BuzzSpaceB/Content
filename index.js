86d1e59e30b2826c828852bc1475ee02a5df39c7
u13073878 <u13073878@tuks.co.za>
2015-04-02 17:51:54 +0200

    Removed server and started the index.js file

---
+++ index.js
@@ -0,0 +1,52 @@
+/**
+ * Contents module for D3
+ *
+ *
+ */
+
+var reporting = require('Reporting');
+var resources = require('Resources');
+var status = require('Status');
+var threads = require('Threads');
+
+//Reporting
+
+
+// Resources
+
+function uploadResources()
+{
+
+}
+
+function removeResources()
+{
+
+}
+
+// Status
+
+// Threads
+
+module.export.uploadResources = uploadResources;
+
+module.export.removeResources = function()
+{
+
+}
+
+module.export.addResourceType = function()
+{
+
+
+}
+
+module.export.removeResourceType = function()
+{
+
+}
+
+module.export.modifyResourceType = function()
+{
+
+}
