8f3e470304088920c204d74e433c2b05da50f2e3
u13073878 <u13073878@tuks.co.za>
2015-04-10 19:24:58 +0200

    Removed unnecessary models

--- models/subscription.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var subscriptionSchema = mongoose.Schema({
-    User_id: String,
-	registeredTo: [String],
-	Thread_id: String
-});
-
-module.exports = mongoose.model("subscriptions", SubscriptionShcema);
