8b140837c4dd2c442ca4aff41ba1748188a3bafa
Frikkie Snyman <frikkie.snyman@live.co.za>
2015-04-16 12:58:24 +0200

    Started work on integration tests using nodeunit

---
+++ Integration Tests/test.js
@@ -0,0 +1,21 @@
+var content = require('../Content');
+
+// Install via npm: npm install nodeunit -g
+
+
+// Run using command: nodeunit test.js
+
+// Here is an example unit test module:
+
+// exports.testSomething = function(test){
+//     test.expect(1);
+//     test.ok(true, "this assertion should pass");
+//     test.done();
+// };
+
+// exports.testSomethingElse = function(test){
+//     test.ok(false, "this assertion should fail");
+//     test.done();
+// };
+
+// View https://github.com/caolan/nodeunit for usage please
